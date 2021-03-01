103c582ebbc6a72c3d0d1e17897e92e392382fc3
CJeezy <73773262+CJeezy@users.noreply.github.com>
2021-02-28 19:15:35 -0500

    final build. assignment complete

--- app/components/base-assignment.js
+++ app/components/base-assignment.js
@@ -4,8 +4,9 @@ export default class BaseAssignmentComponent extends Component {
 
     numArray = [1,2,3,4,5,6,7,8,9,10]
     errorArray = [1,2, 'this should cause an error', 3, 4, 'this too', 5]
-    num1 = 1
-    num2 = 5
-    num3 = 5
-    num4 = 10
+
+    num1 = 2
+    num2 = 2
+    num3 = 3
+    num4 = 4
 }
--- app/helpers/gte.js
+++ app/helpers/gte.js
@@ -2,7 +2,7 @@ import { helper } from "@ember/component/helper"
 
 var gte = ([num1, num2]) => {
     if (num1 >= num2) return true
-    else false
+    else return false
 
 }
 
